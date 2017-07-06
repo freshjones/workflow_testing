15b53885b6259dbfb4a6e20160f2bd8efad81e15
William Jones <billy@freshjones.com>
2017-07-06 06:48:14 -0400

    adding dev env data

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,20 @@ var server = require('./server');
 
 const PORT = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack')
+  const webpackDevMiddleware = require('webpack-dev-middleware')
+  const webpackHotMiddleware = require('webpack-hot-middleware')
+  const config = require('../webpack.deployment.config.js')
+  const compiler = webpack(config)
+
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPathdist
+  }))
+}
+
 server.listen(PORT, function () {
   console.log('Server listening on', PORT);
 });
